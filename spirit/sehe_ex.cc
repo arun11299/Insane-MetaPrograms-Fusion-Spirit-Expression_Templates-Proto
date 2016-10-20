e346c588165434285e68cfda9659c3ef7085c353
Arun M <amuralid@Prashanths-MBP.blr.riftio.com>
2016-10-20 16:21:12 +0530

    new spirit examples

---
+++ spirit/sehe_ex.cc
@@ -0,0 +1,67 @@
+#define BOOST_SPIRIT_X3_DEBUG
+#include <iostream>
+#include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+
+namespace x3 = boost::spirit::x3;
+
+namespace ast{
+    struct LambdaType;
+    struct ClassType;
+    struct nil{};
+
+    typedef x3::variant<
+        nil,
+        x3::forward_ast<LambdaType>,
+        x3::forward_ast<ClassType>
+    > Type;
+
+    struct LambdaType {
+        std::vector<Type> parameters_;
+        Type return_type_;
+    };
+
+    struct ClassType{
+        std::vector<std::string> name_; 
+        std::vector<Type> template_args_;
+    };
+
+}
+
+BOOST_FUSION_ADAPT_STRUCT(ast::LambdaType, parameters_, return_type_)
+BOOST_FUSION_ADAPT_STRUCT(ast::ClassType, name_, template_args_)
+
+namespace parser{
+    typedef x3::rule<struct lambda_type_class, ast::LambdaType> lambda_type_type;
+    typedef x3::rule<struct class_type_class,  ast::ClassType>  class_type_type;
+    typedef x3::rule<struct type_class,        ast::Type>       type_type;
+
+    const class_type_type  class_type  = "class_type";
+    const lambda_type_type lambda_type = "lambda_type";
+    const type_type        type_p      = "type";
+
+    auto const type_p_def = class_type | lambda_type;
+
+    auto const lambda_type_def =
+        ("(" >> -(type_p%",") >> ")" >> "=>" >> type_p)
+        | (x3::repeat(1)[type_p%","] >> "=>" >> type_p)
+            ;
+
+    auto const class_type_def =
+            (x3::string("foo")%"::") >> -("<" >> type_p%"," >> ">")
+            ;
+
+    BOOST_SPIRIT_DEFINE(
+            lambda_type,
+            class_type,
+            type_p
+    )
+}
+
+int main()
+{
+    std::string input = "(foo, foo) => foo";
+    x3::phrase_parse(input.begin(), input.end(), parser::type_p, x3::space);
+}
+
---
+++ spirit/x3_fun-master/test/eval_expression_test.cpp
@@ -0,0 +1,88 @@
+/*=============================================================================
+    Copyright (c) 2001-2015 Joel de Guzman
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+
+#include "../fun/ast.hpp"
+#include "../fun/expression.hpp"
+#include "../fun/error_handler.hpp"
+#include "../fun/config.hpp"
+#include "../fun/interpreter.hpp"
+
+#include <boost/spirit/home/x3/support/utility/testing.hpp>
+
+namespace fs = boost::filesystem;
+namespace testing = boost::spirit::x3::testing;
+
+auto parse = [](std::string const& source, fs::path input_path)-> std::string
+{
+    std::stringstream out;
+
+    using fun::parser::iterator_type;
+    iterator_type iter(source.begin());
+    iterator_type end(source.end());
+
+    // Our AST
+    fun::ast::expression ast;
+
+    // Our error handler
+    using boost::spirit::x3::with;
+    using fun::parser::error_handler_type;
+    error_handler_type error_handler(iter, end, out, input_path.c_str()); // Our error handler
+
+    // Our parser
+    auto const parser =
+        // we pass our error handler to the parser so we can access
+        // it later on in our on_error and on_sucess handlers
+        with<fun::parser::error_handler_tag>(std::ref(error_handler))
+        [
+            fun::expression()
+        ];
+
+    // Our Interpreter
+    fun::ast::interpreter interp(error_handler);
+
+    // Add some functions:
+    interp.add_function("pi", []{ return M_PI; });
+    interp.add_function("sin", [](double x){ return std::sin(x); });
+    interp.add_function("cos", [](double x){ return std::cos(x); });
+
+    // Go forth and parse!
+    using boost::spirit::x3::ascii::space;
+    bool success = phrase_parse(iter, end, parser, space, ast);
+
+    if (success)
+    {
+        if (iter != end)
+            return "Error! Expecting end of input here: " + std::string(iter, end) + '\n';
+        double result = interp.eval(ast);
+        if (out.str().empty())
+            out << result << std::endl;
+    }
+
+    return out.str();
+};
+
+auto compare = [](fs::path input_path, fs::path expect_path)
+{
+   testing::compare(input_path, expect_path, parse);
+};
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+       std::cout << "usage: " << fs::path(argv[0]).filename() << " path/to/test/files" << std::endl;
+       return -1;
+    }
+
+    return testing::for_each_file(fs::path(argv[1]), compare);
+}
